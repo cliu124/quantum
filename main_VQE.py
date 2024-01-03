ff6d65aa9e6de7d86420d6ad60bd85efa6df85bb
cliu124 <39631690+cliu124@users.noreply.github.com>
2024-01-02 22:58:54 -0800

    Update to a general symmetric matrix

--- main_VQE.py
+++ main_VQE.py
@@ -2,7 +2,9 @@ import numpy as np
 from qiskit_algorithms import VQD, NumPyEigensolver, optimizers
 from qiskit.quantum_info.operators import Operator
 from qiskit.quantum_info import SparsePauliOp
-from qiskit.circuit.library import TwoLocal
+#from qiskit.circuit.library import TwoLocal
+#from qiskit.circuit.library import NLocal
+from qiskit.circuit.library import EfficientSU2
 from qiskit.primitives import Sampler, Estimator
 from qiskit_algorithms.state_fidelities import ComputeUncompute
 from qiskit import Aer
@@ -25,11 +27,15 @@ Hamil[1, 2] = c1
 Hamil[2, 0] = c1
 Hamil[2, 1] = c1
 
+#construct a Hermitian matrix
+#Hamil_real = np.random.randn((n,n))
+#Hamil_imag = np.random.randn((n,n))
+Hamil = Hamil + np.transpose(Hamil)
 
 print("(4x4) Hamiltonian")
 print(Hamil)
-solver = NumPyEigensolver(k=4)
-Hamil_Mat = Operator(Hamil, 4)
+solver = NumPyEigensolver(k=n)
+Hamil_Mat = Operator(Hamil, n)
 classical_results = solver.compute_eigenvalues(Hamil_Mat)
 print("Qubit Op Eigenvalues: ")
 print(classical_results.eigenvalues)
@@ -41,7 +47,13 @@ print(eigenvalues)
 # Create a unitary operator from the hamiltonian matrix
 Hamil_Qop = SparsePauliOp.from_operator(Hamil_Mat)
 print(Hamil_Qop)
-ansatz = TwoLocal(2, rotation_blocks=["ry", "rz"], entanglement_blocks="cz", reps=1)
+
+#Old version ansatz, from Kalin
+#ansatz = TwoLocal(2, rotation_blocks=["ry", "rz"], entanglement_blocks="cz", reps=1)
+
+#from https://learning.quantum.ibm.com/tutorial/variational-quantum-eigensolver
+#work for arbitrary qubit numbers
+ansatz = EfficientSU2(Hamil_Qop.num_qubits)
 
 optimizer = optimizers.SLSQP()
 ansatz.decompose().draw("mpl")
@@ -60,7 +72,7 @@ def callback(eval_count, params, value, meta, step):
 
 
 # Create a circuit that uses this operator as a unitary circuit element
-vqd = VQD(estimator, fidelity, ansatz, optimizer, k=4,callback=callback)
+vqd = VQD(estimator, fidelity, ansatz, optimizer, k=n,callback=callback)
 result = vqd.compute_eigenvalues(operator = Hamil_Qop)
 vqd_values = result.eigenvalues
 print(vqd_values)
