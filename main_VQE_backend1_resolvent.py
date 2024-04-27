3ff101992b517e50a1df72aeaaeec43d1b9a2648
Chang Liu @ UConn ME <chang_liu@uconn.edu>
2024-04-27 15:48:58 -0400

    github

--- main_VQE_backend1_resolvent.py
+++ main_VQE_backend1_resolvent.py
@@ -19,8 +19,9 @@ from poly_diff import Chebyshev
 
 ###simulator
 #backend = Aer.get_backend("qasm_simulator")
-QiskitRuntimeService.save_account(channel="ibm_quantum", token="3e240c42418c07b80ef72d580d6074ef560da4546167a9d2ad9591c0f845526e71e03f2d0fa393a5302212d30b77e406e744cc55bd7e4e22670f095d1f8791d0",overwrite=True)
-service = QiskitRuntimeService(channel='ibm_quantum')
+#QiskitRuntimeService.save_account(channel="ibm_quantum", token="3e240c42418c07b80ef72d580d6074ef560da4546167a9d2ad9591c0f845526e71e03f2d0fa393a5302212d30b77e406e744cc55bd7e4e22670f095d1f8791d0",overwrite=True)
+#service = QiskitRuntimeService(channel='ibm_quantum')
+service = QiskitRuntimeService(name="UConn_quantum_credit",instance="UConn")
 backend = service.least_busy(operational=True, simulator=False)
 print(backend.name)
 
